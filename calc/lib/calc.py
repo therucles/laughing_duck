2c3fd1afa55b3841c7224133f634fd7dce6ad534
Cory Weinstine <therucles@yahoo.com>
2018-02-06 00:38:46 -0700

    Fixed variables

--- calc/lib/calc.py
+++ calc/lib/calc.py
@@ -1,16 +1,16 @@
-def add(x,y):
+def add(x, y):
     return x + y
 
 
-def subtract(x,y):
+def subtract(x, y):
     return x - y
 
 
-def multiply(x,y):
+def multiply(x, y):
     return x * y
 
 
-def divide(x,y):
+def divide(x, y):
     return x / y
 
 
@@ -18,5 +18,5 @@ def squared(x):
     return x ** 2
 
 
-def exponent(x,y):
+def exponent(x, y):
     return x ** y
